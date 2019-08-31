5c98df989d95fb3b7190ef3d4e3cff6fee952976
moos <mooster@42at.com>
2019-08-30 22:04:45 -0700

    Initial checkin to git

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # iranica-reader
-Chrome extension for making Iranica articles easier to read online!
+Chrome extension for making [http://www.iranicaonline.org/](Encyclopædia Iranica) articles easier to read online.
+
+Find it in Chrome Stores (coming soon).
+
+## List of features
+
+* Removes noisy transliteration diacritics (using [https://github.com/dundalek/latinize](latinize))
+* Makes section headers clearly visible
+* Moves in-text citation to linked footnote
+* Adds collapsible footnotes section at the end
+* Collapse date and date ranges to only show Gregorian dates (Islamic dates are shown on mouse hover)
+* Splits long paragraphs
+
+### Features in the works
+* Lightbox images (no page change)
+* Options page (maybe?)
+
+## Caveats
+
+Much of the reformatting is done via regular expressions and not all the articles follow a strict format or have correct markup.  
+
+If you notice something odd, temporarily disable the extension, or open the page in incognito mode.
+
+## License
+
+MIT 
---
+++ src/inject/inject.js
@@ -0,0 +1,173 @@
+/**
+ * iranica-reader
+ *
+ * (c) Copyright 2019, mooster@42at.com
+ */
+
+
+chrome.extension.sendMessage({}, function(response) {
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
+      run();
+    }
+  }, 100);
+});
+
+
+function run() {
+  let $$ = document.querySelectorAll.bind(document);
+  let article = document.querySelector('#article-overview');
+  if (!article) return;
+
+  document.body.className += ' iranica-reader';
+  console.log('Iranica reader loaded for:',
+    (article.textContent || '').trim()
+  );
+
+  // extend latinize
+  latinize.characters['š'] = 'sh';
+
+  let contentPs = $$('#content p');
+
+  //
+  // section headers
+  //  <p><em>foo</em>.  content....
+  //  <p><a name></a>fooo</p>
+  //
+  let bib;
+  contentPs.forEach(p => {
+    let em = p.firstChild;
+    if (!em ||
+      !(em.nodeName === 'EM' || (em.nodeName === 'A' && em.attributes.name)) ||
+      (/\(/.test(em.textContent) && !/\)/.test(em.textContent)) // mismatched ()
+    ) return;
+
+    var newEl = document.createElement('h2');
+    newEl.innerHTML = em.innerHTML || p.innerHTML;
+
+    console.log(111, newEl.textContent.length, newEl.textContent)
+    if (newEl.textContent.length > 150) return;
+
+    if (!em.innerHTML) {
+      p.innerHTML = '';
+      p.appendChild(newEl);
+    } else {
+      p.replaceChild(newEl, em);
+      p.innerHTML = p.innerHTML.replace(/h2>\.\s*/, 'h2>'); // remove leading period
+    }
+
+    if (/bibliography/i.test(em.innerText)) bib = p;
+  });
+
+
+  //
+  // citations of type (...<em>...</em>...) or (... pp?. ...) not containing <a>
+  //
+  let notes = [];
+  contentPs.forEach(p => {
+    p.innerHTML = p.innerHTML.replace(/\((.*?)\)/g, (a, m) => {
+      let orig = `(${m})`;
+
+      if (
+        /<a\b/i.test(m) || // skip <a>'s
+        /^<em.*em>$/i.test(m) // skip (<em>foo</em>)
+      ) {
+        return orig;
+      }
+      if (/<em>/i.test(m) || /\bpp?\./.test(m)) {
+        notes.push(latinize(m)); // latinize
+        let n = notes.length;
+        return `<sup>[<a href="#notes-${n}"><code>${n}</code></a>]</sup>`;
+      }
+      return orig;
+    });
+  });
+
+
+  //
+  // add footnotes
+  //
+  if (!bib) console.warn('No bibliography section found!');
+  else {
+    let str = '';
+    notes.forEach((note, n) => {
+      note = note.replace(/^./, m => m.toUpperCase());
+      str += `<li><a name="notes-${1+n}"></a>${note}</li>`;
+    });
+    let ol = document.createElement('ol');
+    ol.innerHTML = str;
+
+    let newbib = document.createElement('details');
+    newbib.setAttribute('open', '');
+    newbib.innerHTML = `<summary><b>Footnotes</b></summary>`;
+    bib.parentNode.insertBefore(newbib, bib);
+    newbib.appendChild(ol);
+  }
+
+  //
+  // latinize main text and break up long paragraphs
+  //
+  contentPs.forEach(p => {
+    p.innerHTML = splitLongPs(latinize(p.innerHTML));
+  });
+
+  //
+  // dates:
+  //
+  contentPs.forEach(p => {
+    // 1265/1840
+    // 1265-78/140-50
+    let html = p.innerHTML;
+    html = html.replace(
+      /(?<hd>\d{4}-?(\d{2,})?)\/(?<gd>\d{4}-?(\d{2,})?)/g,
+      '<abbr title="$<hd>">$<gd></abbr>');
+    // 22 Farvardin 1209/11 September 1830
+    // Farvardin, 1209/September, 1830
+    // Rabiʿ II, 1309/...
+    html = html.replace(
+      /(?<hd>\b(\d{1,2}\s+)?[A-Z]\w+(ʿ?\s*I+)?(,?\s+)?\d{4}-?(\d{2,})?)\/(?<gd>\b(\d{1,2}\s+)?[A-Z]\w+(,?\s+)?\d{4}-?(\d{2,})?)/g,
+      '<abbr title="$<hd>">$<gd></abbr>');
+
+    p.innerHTML = html;
+  });
+
+  //
+  // add citation as title
+  // title="${t.textContent}"
+  //
+  $$('#content p sup').forEach(p => {
+    let c = p.querySelector('code');
+    if (!c) return; // not our <sup>
+    let n = c.textContent - 1;
+    let a = p.querySelector('a');
+    let t = document.createElement('span');
+    t.innerHTML = notes[n];
+    a.setAttribute('title', t.textContent);
+  });
+
+  // TODO
+  //   images are lightbox
+
+  addExtMarker();
+}
+
+
+
+function addExtMarker() {
+  let marker = document.createElement('div');
+  marker.innerHTML = 'Iranica Reader mode';
+  marker.className = 'iranica-reader-marker';
+  marker.setAttribute('title', 'To see original view, turn off the Iranica Reader extension in your browser.')
+  document.body.appendChild(marker);
+}
+
+function splitLongPs(html, N = 5) {
+  let i = 0;
+  return html
+    .split(/\.\s+(?=[A-Z])/)
+    .map(t => {
+      return t.length < 50 || /<\/?sup>/.test(t) || (++i % N) ? t : (i = 0, `<br/><br/>${t}`);
+    })
+    .join('. ');
+};
